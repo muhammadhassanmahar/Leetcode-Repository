d52e922b31419744ab4295705fbad82622766e6e
Muhmmad Hassan <immaharhasaan@gmail.com>
2026-07-28 15:36:57 +0500

    ✅ Smallest Palindromic Rearrangement I | Medium | Python | Runtime: 436 ms (61.76%) | Memory: 13.3 MB | 2026-07-28

---
+++ medium/3517-smallest-palindromic-rearrangement-i/solution.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def smallestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        freq = [0] * 26
+
+        for ch in s:
+            freq[ord(ch) - ord('a')] += 1
+
+        left = []
+        mid = ""
+
+        for i in range(26):
+            if freq[i] % 2 == 1:
+                mid = chr(i + ord('a'))
+            left.append(chr(i + ord('a')) * (freq[i] // 2))
+
+        left = "".join(left)
+        return left + mid + left[::-1]
