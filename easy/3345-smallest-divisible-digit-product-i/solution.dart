c7a9de0917dfbb88e764f33acbb3232cbaf43bb2
Muhmmad Hassan <immaharhasaan@gmail.com>
2026-08-06 11:14:28 +0500

    ✅ Smallest Divisible Digit Product I | Easy | dart | Runtime: 0 ms (100.00%) | Memory: 153.7 MB | 2026-08-06

---
+++ easy/3345-smallest-divisible-digit-product-i/solution.dart
@@ -0,0 +1,19 @@
+class Solution {
+  int smallestNumber(int n, int t) {
+    while (true) {
+      int product = 1;
+      int x = n;
+
+      while (x > 0) {
+        product *= x % 10;
+        x ~/= 10;
+      }
+
+      if (product % t == 0) {
+        return n;
+      }
+
+      n++;
+    }
+  }
+}
