d020aa546956e5e31c53c16f64d85ef6795cd246
Muhmmad Hassan <immaharhasaan@gmail.com>
2026-09-06 20:00:20 +0500

    ✅ Odd Even Linked List | Medium | dart | Runtime: 0 ms (100.00%) | Memory: 147.9 MB | 2026-09-06

---
+++ medium/0328-odd-even-linked-list/solution.dart
@@ -0,0 +1,23 @@
+class Solution {
+  ListNode? oddEvenList(ListNode? head) {
+    if (head == null || head.next == null) return head;
+
+    ListNode odd = head;
+    ListNode even = head.next!;
+    ListNode evenHead = even;
+
+    while (even.next != null) {
+      odd.next = even.next;
+      odd = odd.next!;
+
+      even.next = odd.next;
+      if (even.next != null) {
+        even = even.next!;
+      }
+    }
+
+    odd.next = evenHead;
+
+    return head;
+  }
+}
